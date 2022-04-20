929e38dc2a1ed7784c9fd1f75b094e6d39d4363c
timothyVermUCSC <62963623+timothyVermUCSC@users.noreply.github.com>
2022-04-20 11:39:12 -0700

    Lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,19 @@
+// Authors:
+// Timothy Vermeersch tdvermee@ucsc.edu
+// Brent Chou brechou@ucsc.edu
+// Created: 20 April
+
+//Define variables
+make = "Honda"
+model = "Civic Hatch"
+color = "Red"
+year = 1997
+
+// calculate
+age = new Date().getFullYear() - year
+
+document.writeln("Make: " + make + "<br>")
+document.writeln("Model: " + model + "<br>")
+document.writeln("Color: " + color + "<br>")
+document.writeln("Year: " + year + "<br>")
+document.writeln("Age: " + age + "<br>")
